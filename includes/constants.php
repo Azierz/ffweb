ed34642f344214b6a8f968de70e6a78bf566d0e8
arsyad z <travisschafer87@yahoo.com>
2021-11-24 01:53:47 +0800

    login & constant update

--- includes/constants.php
+++ includes/constants.php
@@ -7,7 +7,6 @@
     define('DB_NAME', 'ff');
 
     $dbc = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME) or die(mysqli_error()); //Database Connection
-    //$dbc = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //Selecting Database
 
 
 ?>
--- login.php
+++ login.php
@@ -4,105 +4,88 @@ $page_title = 'Login Form';
 $page_text = 'Login Form';
 include ('includes/header2.php');
 
-// if (!empty($_SESSION['user_id']) || !empty($_SESSION['admin_id'])) {
-// 	echo '
-// 		<script>
-// 		window.alert("\nALREADY LOGGED IN!");
-// 		setTimeout(function(){location.href="profile.php"},0);
-// 		</script>';
-// }
+if (!empty($_SESSION['user_id']) || !empty($_SESSION['admin_id'])) {
+	echo '
+		<script>
+		window.alert("\nALREADY LOGGED IN!");
+		setTimeout(function(){location.href="profile.php"},0);
+		</script>';
+}
 
 
-// // Run after submit form
-// if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-// 	// Make the connection to the database
-// 	require ('mysqli_connect.php');
+// Run after submit form
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	// Make the connection to the database
+	require ('includes/constants.php');
 
-// 	$errors = array(); // Initialize error array.
+	$errors = array(); // Initialize error array.
 
-// 	// Validate the email address:
-// 	if (empty($_POST['username'])) {
-// 		$errors[] = 'You forgot to enter your username.';
-// 	} else {
-// 		$un = mysqli_real_escape_string($dbc, trim($_POST['username']));
-// 	}
+	// Validate the email address:
+	if (empty($_POST['email'])) {
+		$errors[] = 'You forgot to enter your email.';
+	} else {
+		$em = mysqli_real_escape_string($dbc, trim($_POST['email']));
+	}
 
-// 	// Validate the password:
-// 	if (empty($_POST['pass'])) {
-// 		$errors[] = 'You forgot to enter your password.';
-// 	} else {
-// 		$p = mysqli_real_escape_string($dbc, trim($_POST['pass']));
-// 	}
+	// Validate the password:
+	if (empty($_POST['pass'])) {
+		$errors[] = 'You forgot to enter your password.';
+	} else {
+		$p = mysqli_real_escape_string($dbc, trim($_POST['pass']));
+	}
 
-// 	if (empty($errors)) { // If everything's OK.
+	if (empty($errors)) { // If everything's OK.
 
-// 		// Query for admin
-// 		$q = "SELECT admin_id, username FROM admin WHERE username='$un' AND password=SHA1('$p')";
-// 		$r = @mysqli_query ($dbc, $q); // Run the query.
+		// Query for user
+		$q = "SELECT CustID, CustName, Email FROM customer WHERE Email='$em' AND password=SHA1('$p')";
+		$r = @mysqli_query ($dbc, $q); // Run the query.
 
-// 		// Query for user
-// 		$q1 = "SELECT user_id, username, profile_pic FROM users WHERE username='$un' AND password=SHA1('$p')";
-// 		$r1 = @mysqli_query ($dbc, $q1); // Run the query.
+		// Check the result:
+		if (mysqli_num_rows($r) == 1) {
 
-// 		// Check the result:
-// 		if (mysqli_num_rows($r) == 1) {
+			// Fetch the record:
+			$row = mysqli_fetch_array($r, MYSQLI_ASSOC);
 
-// 			// Fetch the record:
-// 			$row = mysqli_fetch_array($r, MYSQLI_ASSOC);
+			// Set the session data:
+			session_start();
+			$_SESSION['user_id'] = $row['user_id'];
+			$_SESSION['email'] = $em;
+			$_SESSION['profile_pic'] = $row['profile_pic'];
 
-// 			// Set the session data:
-// 			session_start();
-// 			$_SESSION['admin_id'] = $row['admin_id'];
-// 			$_SESSION['username'] = $un;
+			// Redirect user
+			header("Location:index.php");
 
-// 			// Redirect user
-// 			header("Location:profile.php");
+		} else { // Not a match!
+			$errors[] = 'The email and password entered do not match those on file.';
+		}
+	} // End of empty($errors) IF.
 
-// 		} elseif (mysqli_num_rows($r1) == 1) {
+	mysqli_close($dbc); // Close the database connection.
 
-// 			// Fetch the record:
-// 			$row = mysqli_fetch_array($r1, MYSQLI_ASSOC);
+	if ($errors) { // Report the errors.
+		echo '<h1>Error!</h1>
+		<div id ="errors">The following error(s) occurred:<br />';
+		foreach ($errors as $msg) { // Print each error.
+			echo " - $msg<br/>";
+		}
+		echo '</div>
+		<div id = "errors">Please try again.</div>'; // Close div "errors"
+	}
 
-// 			// Set the session data:
-// 			session_start();
-// 			$_SESSION['user_id'] = $row['user_id'];
-// 			$_SESSION['username'] = $un;
-// 			$_SESSION['profile_pic'] = $row['profile_pic'];
-
-// 			// Redirect user
-// 			header("Location:profile.php");
-
-// 		} else { // Not a match!
-// 			$errors[] = 'The username and password entered do not match those on file.';
-// 		}
-// 	} // End of empty($errors) IF.
-
-// 	mysqli_close($dbc); // Close the database connection.
-
-// 	if ($errors) { // Report the errors.
-// 		echo '<h1>Error!</h1>
-// 		<div id ="errors">The following error(s) occurred:<br />';
-// 		foreach ($errors as $msg) { // Print each error.
-// 			echo " - $msg<br/>";
-// 		}
-// 		echo '</div>
-// 		<div id = "errors">Please try again.</div>'; // Close div "errors"
-// 	}
-
-// } // End of the main submit conditional.
+} // End of the main submit conditional.
 
 // Display the form:?>
 <h1>LOGIN FORM</h1>
 <form action="login.php" method="post">
 	<table style="font-size: 100%">
 		<tr>
-			<td><p>Username</p></td>
-			<td><p><input type="text" name="username" size="20"/></p></td>
+			<td><p>E-Mail</p></td>
+			<td><p><input type="email" name="email" size="60"/></p></td>
 		</tr>
 
 		<tr>
 			<td><p>Password</p></td>
-			<td><p><input type="password" name="pass" size="20"/></p></td>
+			<td><p><input type="password" name="pass" size="10"/></p></td>
 		</tr>
 	</table>
 	<p align="right"><input type="submit" name="submit" value="Login" /></p>
