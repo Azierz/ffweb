d453cc3c52d850ac5f9c795b199dccf48a86c327
Azizi <mr.krab72@gmail.com>
2021-11-23 20:57:26 +0800

    header php update

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title><?php echo $page_title; ?></title>
+	<link rel="stylesheet" href="includes/style.css" type="text/css" media="screen"
+/>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+</head>
+<body>
+	<div id="header">
+		<img src = "includes/fff.jpg" style="width: 100%; height: auto;">
+		<h1>Fruity Fruit </h1>
+		<h2><?php echo $page_text; ?> </h2>
+	</div>
+	<div id="navigation">
+		<ul>
+
+
+			<li><a href="view_users.php">View Users</a></li>
+			<li><a href="num.php">Compare Number</a></li>
+			<li><a href="calc.php">BMI Calculator</a></li>
+			<li><a href="logout.php">Logout</a></li>';
+
+		</ul>
+	</div>
+	<div id="content">
