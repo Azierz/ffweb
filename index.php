7881d6025b6fc96fcae6527f56fc1414c89f1013
arsyad z <travisschafer87@yahoo.com>
2021-11-24 03:18:00 +0800

    remove dot

--- index.php
+++ index.php
@@ -27,12 +27,6 @@ include ('includes/header2.php');
 
 <br>
 
-<div style="text-align:center">
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-</div>
-
 <script>
 var slideIndex = 0;
 showSlides();
@@ -40,17 +34,12 @@ showSlides();
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 </script>
