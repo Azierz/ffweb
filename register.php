b50c590a2b9e48c63b17ccb623c348c755d9f7eb
Arsyad <mohdarsyad.zaini@yahoo.com>
2021-11-23 20:05:49 +0800

    reg.php edit

--- register.php
+++ register.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // This script performs an INSERT query to add a record to the users table.
 
 $page_title = 'Register';
@@ -8,14 +8,14 @@ include ('includes/header2.html');
 // if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 // 	$errors = array(); // Initialize an error array.
-	
+
 // 	// Check for a first name:
 // 	if (empty($_POST['first_name'])) {
 // 		$errors[] = 'You forgot to enter your first name.';
 // 	} else {
 // 		$fn = trim($_POST['first_name']);
 // 	}
-	
+
 // 	// Check for a last name:
 // 	if (empty($_POST['last_name'])) {
 // 		$errors[] = 'You forgot to enter your last name.';
@@ -25,11 +25,11 @@ include ('includes/header2.html');
 
 // 	// Validate the phone no:
 // 	if (empty($_POST['phone_no'])) {
-		
+
 // 		$errors[] = 'You forgot to enter your phone number.';
 // 	} else {
 // 		$pn = trim($_POST['phone_no']);
-		
+
 // 	}
 
 // 	// Check for an email address:
@@ -52,41 +52,41 @@ include ('includes/header2.html');
 
 // 	$prg = trim($_POST['Programme']);
 
-	
+
 // 	if (empty($errors)) { // If everything's OK. ($fn && $ln && $e)
-	
+
 // 		// Register the user in the database...
-		
+
 // 		require ('mysqli_connect.php'); // Connect to the db.
 
 // 		// Make the query:
-// 		$q = "INSERT INTO student (first_name, last_name, phone_no, email, Programme, password, registration_date) VALUES ('$fn', '$ln', '$pn', '$e', '$prg', SHA1('$p'), NOW() )";		
+// 		$q = "INSERT INTO student (first_name, last_name, phone_no, email, Programme, password, registration_date) VALUES ('$fn', '$ln', '$pn', '$e', '$prg', SHA1('$p'), NOW() )";
 // 		$r = mysqli_query ($dbc, $q); // Run the query.
 // 		if ($r) { // If it ran OK.
 
 // 			// Print a message:
 // 			echo '<h1>Thank you!</h1>
-// 		<p>You are now registered.</p>';	
+// 		<p>You are now registered.</p>';
 
 // 		} else { // If it did not run OK.
-	
+
 // 			// Public message:
 // 			echo '<h1>System Error</h1>
-// 			<p class="error">You could not be registered due to a system error. We apologize for any inconvenience.</p>'; 
-	
+// 			<p class="error">You could not be registered due to a system error. We apologize for any inconvenience.</p>';
+
 // 			// Debugging message:
 // 			echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' . $q . '</p>';
-				
+
 // 		} // End of if ($r) IF.
-		
+
 // 		mysqli_close($dbc); // Close the database connection.
-		
+
 // 		// Include the footer and quit the script:
-// 		include ('includes/footer.html'); 
+// 		include ('includes/footer.html');
 // 		exit();
-		
+
 // 	} else { // Report the errors.
-	
+
 // 		echo '<h1>Error!</h1>
 // 		<div id = "errors">The following error(s) occurred:<br />';
 // 		foreach ($errors as $msg) { // Print each error.
@@ -94,12 +94,13 @@ include ('includes/header2.html');
 // 		}
 // 		echo '</div>
 // 		<div id = "errors">Please try again.</div> <p><br /></p>';
-		
+
 // 	} // End of if (empty($errors)) IF.
 
 // } // End of the main Submit conditional.
 ?>
 <h1>Register</h1>
+<h2>Test</h2>
 <form action="register.php" method="post">
 	<p>Full Name: <input type="text" name="name" size="15" maxlength="20" value="<?php if (isset($_POST['first_name'])) echo $_POST['first_name']; ?>" /></p>
 	<p>Password: <input type="password" name="pass1" size="10" maxlength="20" value="<?php if (isset($_POST['pass1'])) echo $_POST['pass1']; ?>"  /></p>
