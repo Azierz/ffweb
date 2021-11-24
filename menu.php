4dd2953ad67cd88375438990389c49ef1ff531ee
arsyad z <travisschafer87@yahoo.com>
2021-11-24 15:21:18 +0800

    add 4 img, upd8 style n menu

--- menu.php
+++ menu.php
@@ -17,42 +17,45 @@ include ('includes/header2.php');
 	<p>4. BMI Calculator </p>
 
 	<p><i>** Please LOGIN to access all the pages</i></p>
-
-<table id="menu">
-	<tr>
-		<th>Fruit(s):</th>
-		<th>Price:</th>
-		<th>Quantity</th>
-		<th>Add To Cart</th>
-	</tr>
-	<?php
-	require ('includes/constants.php');
-
-	$q = "SELECT * FROM product";
-	$r = @mysqli_query ($dbc,$q);
-
-	if (!mysqli_num_rows($r) == 1) {
-		echo '<tr><td colspan="3">ALL PRODUCT OUT OF STOCK</td></tr>';
-	} else {
-	while ($data = mysqli_fetch_array($r)) {
-		echo "
+<div class="menu">
+	<table border="1">
 		<tr>
-			<td>".$data['Name']."</td>
-			<td>RM ".$data['Price']."</td>
-			<td>".$data['Quantity']."</td>
-			<td>";
-			if ($data['Quantity'] > 0) {
-				echo "<a href=#>See Detail</a>";
-			} else {
-				echo "Out of Stock";
-			}
-			"</td>
+			<th>Fruit(s):</th>
+			<th>Price:</th>
+			<th>Quantity</th>
+			<th>Add To Cart</th>
 		</tr>
-		";
-	}}
-	?>
-</table>
+		<?php
+		require ('includes/constants.php');
 
+		$q = "SELECT * FROM product";
+		$r = @mysqli_query ($dbc,$q);
+
+		if (!mysqli_num_rows($r) == 1) {
+			echo '<tr><td colspan="3">ALL PRODUCT OUT OF STOCK</td></tr>';
+		} else {
+		while ($data = mysqli_fetch_array($r)) {
+			echo "
+			<tr>
+				<td>image</td>
+				<td rowspan='2'>RM ".$data['Price']."</td>
+				<td rowspan='2'>".$data['Quantity']."</td>
+				<td rowspan='2'>";
+				if ($data['Quantity'] > 0) {
+					echo "<a href=#>See Detail</a>";
+				} else {
+					echo "Out of Stock";
+				};
+			echo "</td>
+			</tr>
+			<tr>
+				<td>".$data['Name']."</td>
+			</tr>
+			";
+		}}
+		?>
+	</table>
+</div>
 <?php
 include ('includes/footer.html');
 ?>
