70b1a32a86922983de39e5da1700e5a0989af4f7
Azizi <mr.krab72@gmail.com>
2021-11-25 00:29:56 +0800

    new file

---
+++ checkout.php
@@ -0,0 +1,17 @@
+<?php
+$page_title = 'Checkout';
+$page_text = 'Checkout';
+include ('includes/header2.php');
+?>
+
+<h1>Fruity Fruit Checkout</h1>
+
+		<img src="includes/images/pastel.png" style="width:100%">
+		<div class="centered"><h2>CHECKOUT.</h2>
+			<!-- <br><button class="btn"><a href="menu.php">Shop Now</a></button> -->
+		</div>
+
+
+<?php
+include ('includes/footer.html');
+?>
